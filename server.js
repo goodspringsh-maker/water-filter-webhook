7cafaa6d442cf5de2fdd744253ded31fa643fb0e
goodspringsh-maker <goodspringsh@gmail.com>
2026-06-10 19:20:23 +0800

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  console.log("收到請求:", req.method, req.url);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end("OK");
-}
